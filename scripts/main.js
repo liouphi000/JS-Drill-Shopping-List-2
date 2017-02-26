6fefb9a427ecb914b3102efd7e4548bb1e7d6cf5
Phillip Liou <Phillip Liou>
2017-02-26 11:07:40 -0600

    hopefully did this right

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,48 @@
+var foods = ["pizza","taco","cereal"]
 function makeArray(item1, item2, item3) {
+    return foods
+
   //Should return an array with the three items
 }
 
+var fruits = ["banana", "apple"]
 function addToArray(list, item) {
+  fruits.push(grape)
+
   //Taking an existing array and a new item as input
   //Return an array with the item added to it (hint: .push())
 }
 
 function deleteFromArray(list) {
+  foods.pop()
   //Taking an existing array as input
   //Return an array with the last item removed from it (hint: .pop())
 }
 
-function getLength(list) {
+function getLength(list,list,list) {
+   foods.length;
   //Taking an array as input
   //Return the length of the array
 }
 
-function getFirstItem(list) {
+function getFirstItem(list,list,list) {
+  foods[0] = "Steak"
+
   //Taking an array as input
   //Return the first item of the array
 }
 
 function getLastItem(list){
+    return (foods.slice(-1)[0]);
+
   //Taking an array as input
   //Return the last item of the array
 }
 
 function makeExciting(list) {
+for (var i = 0; i < foods.length; i++) { 
+  return(foods[i] + " !!! "
+}
   //Taking an array as input
   //Create a new, empty array
   //for every item in the input array
